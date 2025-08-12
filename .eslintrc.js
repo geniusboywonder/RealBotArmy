0f01cc243b61d75f9b8ac122c7613857eafab2e9
Neill <nadamson@gmail.com>
2025-08-12 21:12:24 +0200

    Add/update lock files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,27 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2022,
+    ecmaVersion: 2020,
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
+    'plugin:@typescript-eslint/recommended'
   ],
-  root: true,
   env: {
     node: true,
-    jest: true,
+    es6: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist/', 'node_modules/'],
+  ignorePatterns: [
+    'dist/',
+    'logs/',
+    'node_modules/',
+    'scratch/',
+    '*.js'
+  ],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/prefer-const': 'error',
-    '@typescript-eslint/no-var-requires': 'error',
-    'prettier/prettier': 'error',
-    'no-console': 'warn',
-    'no-debugger': 'error',
+    // Add custom rules here
   },
 };
